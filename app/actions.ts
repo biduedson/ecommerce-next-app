46a6baea6842ecad6e83df28a64166cf5bdd524c
Edson <biduzao.bidu21@gmail.com>
2025-01-23 09:30:35 -0300

    feat: create banner picture create and delete pages

--- app/actions.ts
+++ app/actions.ts
@@ -3,7 +3,7 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"; // Gerencia sessões de autenticação no servidor.
 import { redirect } from "next/navigation"; // Redireciona usuários para outras rotas no Next.js.
 import { parseWithZod } from "@conform-to/zod"; // Valida formulários com o Zod.
-import { productSchema } from "./lib/zodSchemas"; // Esquema de validação para os dados do produto.
+import { bannerSchema, productSchema } from "./lib/zodSchemas"; // Esquema de validação para os dados do produto.
 import prisma from "./lib/db"; // Instância do Prisma ORM para interagir com o banco de dados.
 import { Schema } from "zod";
 
@@ -98,6 +98,47 @@ export async function deleteProduct(formData: FormData) {
   await prisma.product.delete({
     where: {
       id: formData.get("productId") as string,
+    },
+  });
+  redirect("/dashboard/products");
+}
+
+export async function createBanner(prevState: any, formData: FormData) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || user.email !== "biduzao.bidu21@gmail.com") {
+    return redirect("/");
+  }
+
+  const submission = parseWithZod(formData, {
+    schema: bannerSchema,
+  });
+
+  if (submission.status !== "success") return submission.reply();
+
+  const banner = await prisma.banner.create({
+    data: {
+      title: submission.value.title,
+      imageString: submission.value.imageString,
+    },
+  });
+
+  redirect("/dashboard/banner");
+}
+
+export async function deleteBanner(formData: FormData) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  // Verifica se o usuário está autenticado e tem permissão para criar produtos.
+  if (!user || user.email !== "biduzao.bidu21@gmail.com") {
+    return redirect("/"); // Redireciona para a página inicial se não estiver autorizado.
+  }
+
+  await prisma.banner.delete({
+    where: {
+      id: formData.get("bannerId") as string,
     },
   });
   redirect("/dashboard/products");
--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -45,6 +45,43 @@ export const ourFileRouter = {
       // O que for retornado aqui será enviado para o callback client-side `onClientUploadComplete`
       return { uploadedBy: metadata.userId };
     }),
+
+  bannerImageRoute: f({
+    image: {
+      /**
+       * Para a lista completa de opções e valores padrão, veja a referência da API de Rotas de Arquivos
+       * @see https://docs.uploadthing.com/file-routes#route-config
+       */
+      maxFileSize: "4MB", // Tamanho máximo do arquivo: 4MB
+      maxFileCount: 1, // Quantidade máxima de arquivos: 1
+    },
+  })
+    // Define permissões e tipos de arquivos para este FileRoute
+    .middleware(async ({ req }) => {
+      // Este código é executado no seu servidor antes do upload
+      //const user = await auth(req);
+
+      // puxando o status de usuario logado em uma função do lado do servidor
+      const { getUser } = getKindeServerSession();
+      const user = await getUser();
+
+      // Se você lançar uma exceção, o usuário não poderá fazer o upload
+      if (!user || user.email !== "biduzao.bidu21@gmail.com")
+        throw new UploadThingError("Não autorizado");
+
+      // O que for retornado aqui estará acessível em `onUploadComplete` como `metadata`
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // Este código É EXECUTADO NO SEU SERVIDOR após o upload
+
+      console.log("Upload concluído para userId:", metadata.userId);
+
+      console.log("URL do arquivo:", file.url);
+
+      // O que for retornado aqui será enviado para o callback client-side `onClientUploadComplete`
+      return { uploadedBy: metadata.userId };
+    }),
 } satisfies FileRouter;
 
 // Tipo que define o FileRouter usado na aplicação
