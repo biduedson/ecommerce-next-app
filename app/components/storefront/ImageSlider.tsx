3e66394346850a7453799cd65ee45ad37c20c7ec
Edson <biduzao.bidu21@gmail.com>
2025-01-24 16:06:12 -0300

    feat: add card product component

---
+++ app/components/storefront/ImageSlider.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
+interface iAppProps {
+  images: string[];
+}
+
+const ImageSlider = ({ images }: iAppProps) => {
+  const [mainImageIndex, setmainImageIndex] = useState(0);
+
+  const handlePreviousClick = () => {
+    setmainImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNextClick = () => {
+    setmainImageIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  return (
+    <div className="grid gap-6 md:gap-3 items-start">
+      <div className="relative overflow-hidden rounded-lg">
+        <Image
+          width={600}
+          height={600}
+          src={images[mainImageIndex]}
+          alt="Product Image"
+          className="object-cover w-[600px] h-[600px] "
+        />
+
+        <div className="absolute inset-0 flex items-center justify-between px-4">
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => handlePreviousClick()}
+          >
+            <ChevronLeft className="w-6 h-6" />
+          </Button>
+          <Button variant="ghost" size="icon" onClick={() => handleNextClick()}>
+            <ChevronRight className="w-6 h-6" />
+          </Button>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-5 gap-4">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            onClick={() => setmainImageIndex(index)}
+            className={`cursor-pointer ${cn(
+              index === mainImageIndex
+                ? "border-2 border-primary"
+                : "border border-gray-200",
+              "relative overflow-hidden rounded-lg"
+            )}`}
+          >
+            <Image
+              src={image}
+              alt="Product Image"
+              width={100}
+              height={100}
+              className="object-cover w-[100px] h-[100px]"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImageSlider;
