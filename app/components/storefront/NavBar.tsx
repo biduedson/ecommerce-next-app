29bf9bcd606da675d6c95422a04f67477eaf5d2f
Edson <biduzao.bidu21@gmail.com>
2025-01-28 12:18:07 -0300

    bug: logout bug on cart

--- app/components/storefront/NavBar.tsx
+++ app/components/storefront/NavBar.tsx
@@ -15,10 +15,13 @@ import { Cart } from "@/app/lib/interfaces";
 const NavBar = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
+  let cart: Cart | null;
+  let total: number | undefined;
+  if (user) {
+    cart = await redis.get(`cart-${user.id}`);
+    total = cart?.items.reduce((sum, item) => sum + item.quantity, 0);
+  }
 
-  const cart: Cart | null = await redis.get(`cart-${user.id}`);
-
-  const total = cart?.items.reduce((sum, item) => sum + item.quantity, 0);
   return (
     <nav className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-5 flex items-center justify-between">
       <div className="flex items-center">
@@ -36,7 +39,7 @@ const NavBar = async () => {
             <Link href="/bag" className="group p-2 flex items-center mr-2">
               <ShoppingBagIcon className="h-6 w-6 text-gray-400 group-hover:text-gray-500" />
               <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-                {total}
+                {total ? total : 0}
               </span>
             </Link>
 
